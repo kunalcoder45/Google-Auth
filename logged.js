76c37fca0a687e85dec26b70d1ad07eefbab1de4
kunalcoder45 <159230510+kunalcoder45@users.noreply.github.com>
2024-08-26 19:56:04 +0530

    Update logged.js

--- logged.js
+++ logged.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCLRgwJCXDz9-NtntVCJDKB5tEPO5WwzAk",
@@ -24,14 +24,24 @@ function updateUserProfile(user) {
   document.getElementById("userProfilePicture").src = userProfilePicture;
 }
 
+// This function checks the authentication state
 onAuthStateChanged(auth, (user) => {
   if (user) {
+    // If user is signed in, update profile UI
     updateUserProfile(user);
-    const uid = user.uid;
-    console.log("User ID:", uid);
-    return uid;
+    console.log("User ID:", user.uid);
   } else {
+    // If no user is signed in, redirect to login page
     alert("Please create an account & log in.");
     window.location.href = "/index.html";
   }
+});
+
+// Ensure that the auth state is being checked after DOM content is loaded
+document.addEventListener("DOMContentLoaded", () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      updateUserProfile(user);
+    }
+  });
 });
